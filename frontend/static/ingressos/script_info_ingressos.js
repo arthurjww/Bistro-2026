38d6e09e5847357164a09d68c6b5d59e03e52951
Guilherme Terres Munaretto da Costa <gtmcosta@ucs.br>
2026-08-24 18:30:53 -0300

    .js de confirmar código feito.

--- frontend/static/ingressos/script_info_ingressos.js
+++ frontend/static/ingressos/script_info_ingressos.js
@@ -1,11 +1,25 @@
+const inputCodigo = document.getElementById('input_codigo');
+const details = document.getElementById('detailIngressos');
+
 const reservado = new Date(
     {{ cronometro.isoformat() | tojson }}
 );
 
+
+cronometroAtualizado();
+
+
+details.addEventListener('click', function (event) {
+    if (details.dataset.podeAbrir === 'false') {
+        event.preventDefault();
+    }
+})
+
+
 function cronometroAtualizado() {
     const agora = new Date();
 
-    const diff = agora - reservado;
+    const diff = 15 * 60 * 1000 - (agora - reservado);
 
     if (diff <= 0) {
         document.getElementById('timer').textContent = '00:00';
@@ -21,6 +35,45 @@ function cronometroAtualizado() {
         `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
 }
 
-cronometroAtualizado();
-
 const intervalo = setInterval(cronometroAtualizado, 1000);
+
+
+
+async function confirmarCodigoAluno() {
+    const payload = {
+        codigo: inputCodigo.value
+    };
+
+    const resposta = await fetch('/info_ingressos/confirmar_codigo', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+    });
+
+    if (resposta.ok) {
+        const dados = await resposta.json();
+        inputCodigo.parentElement.querySelector('span').textContent =
+            `${dados.sucesso}\nApós usar esses ingressos, sobrará ${dados.usos_restantes} usos do código`;
+        details.dataset.podeAbrir = 'true';
+        // dá para colocar mudanças do css aqui
+    }
+}
+
+
+async function enviarDadosESeguirPagamento() {
+    const ingressos = [];
+
+    details.querySelectorAll('form').forEach(form => {
+        const dados = {};
+
+        form.querySelectorAll('input, select, textarea').forEach(input => {
+            dados[input.name] = input.value;
+        });
+
+        ingressos.push(dados);
+    });
+
+    const payload = {
+        ingressos: ingressos
+    };
+}
